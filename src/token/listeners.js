994ff147a8295a3099c134630fcbe4222f697a57
Ross McEwan <rossmcewan@gmail.com>
2018-02-20 17:29:59 +0200

    token transfer basic state machine and listener implementation

--- src/token/listeners.js
+++ src/token/listeners.js
@@ -8,11 +8,11 @@ module.exports.onDalaTokenEvent = (event, context, callback) => {
         if (record.eventName !== 'INSERT') return;
         const newItem = AWS.DynamoDB.Converter.unmarshall(record.dynamodb.NewImage);
         const stateMachineParams = {
-            stateMachineArn: process.env.STATE_MACHINE_ON_TOKEN_TRANSFER,
+            stateMachineArn: process.env.ON_TOKEN_TRANSFER_STATE_MACHINE,
             input: JSON.stringify(newItem),
             name: newItem.id
         };
         return stepFunctions.startExecution(stateMachineParams).promise();
     });
-    return Promise.all(promises).then(() => context.succeed(event)).catch(contex.fail);
+    return Promise.all(promises).then(() => context.succeed(event)).catch(context.fail);
 }
