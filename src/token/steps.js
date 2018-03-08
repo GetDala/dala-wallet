0c0688094c9911a23fef2ffc1bb22328543a6f15
Ross McEwan <rossmcewan@gmail.com>
2018-03-08 15:52:55 +0200

    change DalaTransfer to TokenTransfer and update function names in state machine.

--- src/token/steps.js
+++ src/token/steps.js
@@ -1,18 +1,18 @@
 'use strict';
 
-const DalaTransfer = require('./DalaTransfer');
+const TokenTransfer = require('./TokenTransfer');
 
 module.exports.transferProcessing = (event, context) => {
-    const transfer = new DalaTransfer(event.id);
+    const transfer = new TokenTransfer(event.id);
     transfer.processing().then(context.succeed).catch(context.fail);
 }
 
 module.exports.transferComplete = (event, context) => {
-    const transfer = new DalaTransfer(event.id);
+    const transfer = new TokenTransfer(event.id);
     transfer.complete().then(context.succeed).catch(context.fail);
 }
 
 module.exports.transferFailed = (event, context) => {
-    const transfer = new DalaTransfer(event.id);
+    const transfer = new TokenTransfer(event.id);
     transfer.failed().then(context.succeed).catch(context.fail);
 }
