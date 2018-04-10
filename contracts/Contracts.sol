c9b81c016a28656370094598772283808bf32559
Ross McEwan <rossmcewan@gmail.com>
2018-04-10 14:36:22 +0200

    Changes
    * Changed DalaWallet contract to support setting of destination after
    deployment by owner
    * Enabled paywalls on all http endpoints
    * removed min balance from wallet creation

--- contracts/Contracts.sol
+++ contracts/Contracts.sol
@@ -1,16 +1,16 @@
 pragma solidity ^0.4.17;
 
 contract ERC20 {
-  function balanceOf(address who) public view returns (uint256);
-  function transfer(address to, uint256 value) public returns (bool);
+    function balanceOf(address who) public view returns (uint256);
+    function transfer(address to, uint256 value) public returns (bool);
 }
 
 contract DalaWallet {
     address public owner;
     address public destination;
     address public token;
-    uint256 public minBalance;
-
+    
+    event DestinationChanged(address indexed oldAddress, address indexed newAddress);
     event LogSweep(address indexed from, address indexed to, address indexed token, uint amount);
     
     modifier onlyOwner() {
@@ -19,22 +19,26 @@ contract DalaWallet {
         _;
     }
 
-    function DalaWallet(address _destination, address _token, uint256 _minBalance) {
+    function DalaWallet(address _destination, address _token) public {
         destination = _destination;
         token = _token;
-        minBalance = _minBalance;
         owner = msg.sender;
     }
 
-    function sweep() onlyOwner returns (bool) {
+    function sweep() public onlyOwner returns (bool) {
         var erc20 = ERC20(token);
         var balance = erc20.balanceOf(this);
-        if (balance < minBalance) 
-            revert();
         var success = erc20.transfer(destination, balance);
         if (success) {
             LogSweep(msg.sender, destination, token, balance);
         }
         return success;
+    }
+
+    function setDestination(address _destination) public onlyOwner returns (bool) {
+        var oldDest = destination;
+        destination = _destination;
+        DestinationChanged(oldDest, _destination);
+        return true;
     }
 }
