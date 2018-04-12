95705346add3a0e83c56249e94188d7d89c885a2
Jared Leonard <jaredmyronarthurleonard@gmail.com>
2018-04-12 11:53:30 +0200

    updated the token listeners

--- src/blockchain/token-listener.js
+++ src/blockchain/token-listener.js
@@ -4,7 +4,8 @@ const ProviderEngine = require('web3-provider-engine');
 const Web3Subprovider = require('web3-provider-engine/subproviders/web3.js');
 const FilterSubprovider = require('web3-provider-engine/subproviders/filters.js');
 const AWS = require('aws-sdk');
-const documentClient = new AWS.DynamoDB.DocumentClient({ region: 'eu-west-1' });
+const documentClient = new AWS.DynamoDB.DocumentClient({ region: process.AWS_REGION || 'eu-west-1' });
+var bunyan = require('bunyan');
 
 const RPC_SERVER = process.env.RPC_SERVER;
 const TOKEN_ADDRESS = process.env.TOKEN_ADDRESS;
@@ -19,50 +20,67 @@ engine.start();
 
 const web3 = new Web3(new Web3.providers.HttpProvider(RPC_SERVER));
 
+var loggerDefinition = {
+    name: 'dala-token-listener',
+    INSTANCE_ID: process.env.INSTANCE_ID
+    serializers: { error: bunyan.stdSerializers.err }
+}
+var log = bunyan.createLogger(loggerDefinition);
+
+process.on('exit', (exitCode) => {
+    log.error({ exitCode }, 'dala-token-listener has exited');
+});
+
+process.on('uncaughtException', (error) => {
+    log.error({ error }, error.message);
+})
+
+process.on('unhandledRejection', (reason, p) => {
+    log.error({ reason, p }, 'Unhandled Rejection at:' + p);
+});
+
+
 DalaToken.setProvider(engine);
 DalaToken.at(TOKEN_ADDRESS).then(token => {
     token.Transfer().watch(createEvent);
-}).catch(error=>{
-    console.log(error);
+}).catch(error => {
+    log.error({ error }, 'error occured');
     process.exit(1);
 });
 
 function createEvent(error, event) {
     if (error) {
-        console.log(error);
+        log.error({ error, event }, 'Error occured on create event');
         return;
     }
 
     const payload = {
-        id: event.transactionHash, 
+        id: event.transactionHash,
         timestamp: new Date().toISOString(),
         event
     };
     payload.from = event.args.from;
     payload.to = event.args.to;
     payload.value = web3.fromWei(event.args.value.toString(), 'ether');
-    console.log(payload);
+    log.info({ payload }, 'payload for event');
     var putParams = {
         TableName: 'DalaTokenEvents',
         Item: payload,
         ConditionExpression: 'attribute_not_exists(#id)',
         ExpressionAttributeNames: {
-            '#id':'id'
+            '#id': 'id'
         }
     }
-    console.log('putParams', putParams);
+    log.info({ putParams }, 'putParams for DalaTokenEvents');
     documentClient.put(putParams).promise().then(console.log).catch(handleFailed);
 
     function handleFailed(error) {
-        if(error.code === 'ConditionalCheckFailedException'){
-            console.log('ConditionalCheckFailed');
+        if (error.code === 'ConditionalCheckFailedException') {
+            log.info({ error, event }, 'ConditionalCheckFailed');
             //swallow
-        }else{
-            console.log(error);
+        } else {
+            log.fatal({ error }, 'fatal error occured');
             process.exit(2);
         }
-        //what to do here
-        //don't want to miss an event
-        //give this some thought
     }
 }
