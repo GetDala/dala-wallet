522cbe79bf46a23eceb20d4d3b98b8d01acef78f
Ross McEwan <rossmcewan@gmail.com>
2018-05-07 16:16:36 +0200

    calling API

--- src/accounts/handler.js
+++ src/accounts/handler.js
@@ -1,11 +1,14 @@
 'use strict';
 
 const CognitoUtils = require('../lib/CognitoUtils');
+const api = require('../fineract/api');
 const { getSavingsAccount } = require('../fineract/utils');
 
 module.exports.get = (event, context) => {
+  const savings = api.savings();
   const { address } = event.pathParameters;
   return getSavingsAccount(address)
+    .then(({ savingsId }) => savings.get(savingsId))
     .then(account => {
       console.log(account);
       const summary = account.summary || {};
