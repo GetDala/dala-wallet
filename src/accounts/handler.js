ba0fb5a5658276582021694291318704409b91be
Ross McEwan <rossmcewan@gmail.com>
2018-05-07 15:32:27 +0200

    remove authorizer

--- src/accounts/handler.js
+++ src/accounts/handler.js
@@ -3,27 +3,18 @@
 const CognitoUtils = require('../lib/CognitoUtils');
 const { getSavingsAccount } = require('../fineract/utils');
 
-module.exports.get = (event, context)=>{
-    let { headers, pathParameters } = event;
-    let username;
-    if (headers.paywall) {
-        //decoded parameters should be in the headers
-        username = headers.username;
-    } else {
-        //will need to decode auth header from cognito
-        username = CognitoUtils.getUsernameFromEvent(event);
-    }
-    const {address} = pathParameters;
-    return getSavingsAccount(address, username).then(account=>{
-        const body = JSON.stringify({
-            address,
-            totalDeposits: account.summary.totalDeposits,
-            totalWithdrawals: account.summary.totalWithdrawals,
-            balance: account.summary.balance
-        });
-        return context.succeed({
-            statusCode: 200,
-            body
-        })
-    })
-}
+module.exports.get = (event, context) => {
+  const { address } = event.pathParameters;
+  return getSavingsAccount(address).then(account => {
+    const body = JSON.stringify({
+      address,
+      totalDeposits: account.summary.totalDeposits,
+      totalWithdrawals: account.summary.totalWithdrawals,
+      balance: account.summary.balance
+    });
+    return context.succeed({
+      statusCode: 200,
+      body
+    });
+  });
+};
