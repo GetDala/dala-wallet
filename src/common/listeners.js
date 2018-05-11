bfaa027b14a5c4b88a2c8e1dc65692e515779405
Ross McEwan <rossmcewan@gmail.com>
2018-05-11 14:30:45 +0200

    remove function in serverless. do not fail if ExecutionAlreadyExists error

--- src/common/listeners.js
+++ src/common/listeners.js
@@ -4,21 +4,33 @@ const { EventTypes } = require('./constants');
 const StreamUtils = require('../lib/StreamUtils');
 
 module.exports.onDalaWalletEvent = (event, context) => {
-    return StreamUtils.startStateMachinePerItem(event, {
-        [EventTypes.CreateWallet]: {
-            stateMachineArn: process.env.ON_CREATE_ON_CHAIN_WALLET_STATE_MACHINE
-        },
-        [EventTypes.CreateSubscriber]: {
-            stateMachineArn: process.env.ON_CREATE_SUBSCRIBER_STATE_MACHINE
-        },
-        [EventTypes.UserConfirmed]: {
-            stateMachineArn: process.env.ON_USER_CONFIRMED_STATE_MACHINE
-        },
-        [EventTypes.WebhookReceived]: {
-            stateMachineArn: process.env.ON_WEBHOOK_RECEIVED_STATE_MACHINE
-        },
-        [EventTypes.ExternalTransfer] : {
-            stateMachineArn: process.env.ON_EXTERNAL_TRANSFER_STATE_MACHINE
-        }
-    }).then(() => context.succeed(event)).catch(context.fail);
+  return StreamUtils.startStateMachinePerItem(event, {
+    [EventTypes.CreateWallet]: {
+      stateMachineArn: process.env.ON_CREATE_ON_CHAIN_WALLET_STATE_MACHINE
+    },
+    [EventTypes.CreateSubscriber]: {
+      stateMachineArn: process.env.ON_CREATE_SUBSCRIBER_STATE_MACHINE
+    },
+    [EventTypes.UserConfirmed]: {
+      stateMachineArn: process.env.ON_USER_CONFIRMED_STATE_MACHINE
+    },
+    [EventTypes.WebhookReceived]: {
+      stateMachineArn: process.env.ON_WEBHOOK_RECEIVED_STATE_MACHINE
+    },
+    [EventTypes.ExternalTransfer]: {
+      stateMachineArn: process.env.ON_EXTERNAL_TRANSFER_STATE_MACHINE
+    }
+  })
+    .then(() => context.succeed(event))
+    .catch(error => {
+      console.log(error);
+      if (error.code == 'ExecutionAlreadyExists') {
+        return context.succed({
+          error,
+          message: 'Execution exists. Continuting'
+        });
+      } else {
+        return context.fail(error);
+      }
+    });
 };
--- src/lib/StreamUtils.js
+++ src/lib/StreamUtils.js
@@ -5,7 +5,7 @@ const stepFunctions = new AWS.StepFunctions();
 const uuid = require('uuid');
 
 /**
- * 
+ *
  * @param {Object} event    The DynamoDB event
  * @param {Object} params   The params
  * @param {Object} params[eventType]    The configuration for an event type
@@ -14,21 +14,21 @@ const uuid = require('uuid');
  * @param {Function} params[eventType.getId   Get the id from the new item. If not provided, uses the 'id' property of the new item or 'uuid.v1()' if not present
  */
 module.exports.startStateMachinePerItem = (event, params) => {
-    var promises = event.Records.map(record => {
-        if (record.eventName !== 'INSERT') return;
-        const newItem = AWS.DynamoDB.Converter.unmarshall(record.dynamodb.NewImage);
-        const eventType = newItem.eventType;
-        if (!eventType) return Promise.resolve();
-        const createInput = (params[eventType].createInput) || ((item) => Promise.resolve(item.payload));
-        const getId = params[eventType].getId || ((item) => item.id || uuid.v1());
-        return createInput(newItem).then(input => {
-            const stateMachineParams = {
-                stateMachineArn: params[eventType].stateMachineArn,
-                input: JSON.stringify(input),
-                name: getId(newItem)
-            };
-            return stepFunctions.startExecution(stateMachineParams).promise();
-        });
+  var promises = event.Records.map(record => {
+    if (record.eventName !== 'INSERT') return;
+    const newItem = AWS.DynamoDB.Converter.unmarshall(record.dynamodb.NewImage);
+    const eventType = newItem.eventType;
+    if (!eventType) return Promise.resolve();
+    const createInput = params[eventType].createInput || (item => Promise.resolve(item.payload));
+    const getId = params[eventType].getId || (item => item.id || uuid.v1());
+    return createInput(newItem).then(input => {
+      const stateMachineParams = {
+        stateMachineArn: params[eventType].stateMachineArn,
+        input: JSON.stringify(input),
+        name: getId(newItem)
+      };
+      return stepFunctions.startExecution(stateMachineParams).promise();
     });
-    return Promise.all(promises);
+  });
+  return Promise.all(promises)
 };
