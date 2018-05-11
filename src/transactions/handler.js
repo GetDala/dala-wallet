b9a06b354bf65fca06ae8917702a5dc96c4f706e
Ross McEwan <rossmcewan@gmail.com>
2018-05-11 16:10:09 +0200

    added secrets

--- src/transactions/handler.js
+++ src/transactions/handler.js
@@ -1,36 +1,45 @@
 'use strict';
 
 const Sequelize = require('sequelize');
-const databaseAddress = `postgres://${process.env.STAGING_DATABASE_USERNAME}:${process.env.STAGING_DATABASE_PASSWORD}@${
-  process.env.STAGING_DATABASE_ENDPOINT_ADDRESS
-}:${process.env.STAGING_DATABASE_ENDPOINT_PORT}/${process.env.STAGING_DATABASE}`;
-const sequelize = new Sequelize(databaseAddress);
 const { getSavingsAccount } = require('../fineract/utils');
+const secretsClient = require('serverless-secrets/client');
+const secretsPromise = secretsClient.load();
 
 module.exports.getCount = (event, context) => {
-  let { address } = event.pathParameters;
-  getSavingsAccount(address)
-    .then(account => {
-      if (account) {
-          return sequelize.query(`SELECT COUNT(*) FROM m_savings_account_transactions WHERE savings_account_id = ${account.savingsId}`).then(rows=>{
-              console.log(rows);
-              return context.succeed({
+  return secretsPromise
+    .then(() => {
+      const databaseAddress = `postgres://${process.env.DALA_STORAGE_USERNAME}:${process.env.DALA_STORAGE_PASSWORD}@${
+        process.env.DALA_STORAGE_CLUSTER
+      }:${process.env.STAGING_DATABASE_ENDPOINT_PORT}/${process.env.DALA_STORAGE_PORT}`;
+      const sequelize = new Sequelize(databaseAddress);
+      let { address } = event.pathParameters;
+      getSavingsAccount(address)
+        .then(account => {
+          if (account) {
+            return sequelize
+              .query(`SELECT COUNT(*) FROM m_savings_account_transactions WHERE savings_account_id = ${account.savingsId}`)
+              .then(rows => {
+                console.log(rows);
+                return context.succeed({
                   statusCode: 200,
                   body: JSON.stringify({
-                      address,
-                      transactionCount: rows[0]
+                    address,
+                    transactionCount: rows[0]
                   })
-              });
-          }).catch(context.fail);
-      } else {
-        return context.succeed({
-          statusCode: 200,
-          body: JSON.stringify({
-            address,
-            transactionCount: 0
-          })
-        });
-      }
+                });
+              })
+              .catch(context.fail);
+          } else {
+            return context.succeed({
+              statusCode: 200,
+              body: JSON.stringify({
+                address,
+                transactionCount: 0
+              })
+            });
+          }
+        })
+        .catch(context.fail);
     })
     .catch(context.fail);
 };
