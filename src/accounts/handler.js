eef9c53f6416c62e2b3c60c220ec6b8eeb0590c8
Ross McEwan <rossmcewan@gmail.com>
2018-05-14 16:35:29 +0200

    added getBalance function that does a realtime query on the underlying database

--- src/accounts/handler.js
+++ src/accounts/handler.js
@@ -2,7 +2,11 @@
 
 const CognitoUtils = require('../lib/CognitoUtils');
 const api = require('../fineract/api');
+const Sequelize = require('sequelize');
 const { getSavingsAccount } = require('../fineract/utils');
+const secretsClient = require('serverless-secrets/client');
+const secretsPromise = secretsClient.load();
+
 
 module.exports.get = (event, context) => {
   const savings = api.savings();
@@ -14,8 +18,6 @@ module.exports.get = (event, context) => {
       const summary = account.summary || {};
       const body = JSON.stringify({
         address,
-        totalDeposits: summary.totalDeposits,
-        totalWithdrawals: summary.totalWithdrawals,
         balance: summary.accountBalance
       });
       return context.succeed({
@@ -25,3 +27,43 @@ module.exports.get = (event, context) => {
     })
     .catch(context.fail);
 };
+
+module.exports.getBalance = (event, context)=>{
+  return secretsPromise
+    .then(() => {
+      const databaseAddress = `mysql://${process.env.DALA_STORAGE_USERNAME}:${process.env.DALA_STORAGE_PASSWORD}@${
+        process.env.DALA_STORAGE_CLUSTER
+      }:${process.env.DALA_STORAGE_PORT}/mifostenant-default`;
+      console.log(databaseAddress);
+      const sequelize = new Sequelize(databaseAddress, {
+        operatorsAliases: false
+      });
+      let { address } = event.pathParameters;
+      return getSavingsAccount(address).then(account => {
+        console.log('have savings account', account);
+        if (account) {
+          console.log('calling sequelize');
+          return sequelize.query(`select sum(IF(transaction_type_enum = 2,amount*-1,amount)) as balance from m_savings_account_transaction WHERE savings_account_id = ${account.savingsId}`).then(rows => {
+            console.log(rows);
+            return context.succeed({
+              statusCode: 200,
+              body: JSON.stringify({
+                address,
+                balance: rows[0][0].balance
+              })
+            });
+          });
+        } else {
+          console.log('no account');
+          return context.succeed({
+            statusCode: 200,
+            body: JSON.stringify({
+              address,
+              balance: 'no account'
+            })
+          });
+        }
+      });
+    })
+    .catch(context.fail);
+}
