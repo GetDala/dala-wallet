a9c5c1bf60f7865f781b00b256f786c8654772fc
Ross McEwan <rossmcewan@gmail.com>
2018-06-04 14:16:25 +0200

    add function and path for getTransactions

--- src/transactions/handler.js
+++ src/transactions/handler.js
@@ -11,15 +11,12 @@ module.exports.getCount = (event, context) => {
       const databaseAddress = `mysql://${process.env.DALA_STORAGE_USERNAME}:${process.env.DALA_STORAGE_PASSWORD}@${
         process.env.DALA_STORAGE_CLUSTER
       }:${process.env.DALA_STORAGE_PORT}/mifostenant-default`;
-      console.log(databaseAddress);
       const sequelize = new Sequelize(databaseAddress, {
         operatorsAliases: false
       });
       let { address } = event.pathParameters;
       return getSavingsAccount(address).then(account => {
-        console.log('have savings account', account);
         if (account) {
-          console.log('calling sequelize');
           return sequelize.query(`SELECT COUNT(*) as transactionCount FROM m_savings_account_transaction WHERE savings_account_id = ${account.savingsId}`).then(rows => {
             console.log(rows);
             return context.succeed({
@@ -44,3 +41,45 @@ module.exports.getCount = (event, context) => {
     })
     .catch(context.fail);
 };
+
+module.exports.getTransactions = (event, context)=>{
+  return secretsPromise
+    .then(() => {
+      const databaseAddress = `mysql://${process.env.DALA_STORAGE_USERNAME}:${process.env.DALA_STORAGE_PASSWORD}@${
+        process.env.DALA_STORAGE_CLUSTER
+      }:${process.env.DALA_STORAGE_PORT}/mifostenant-default`;
+      const sequelize = new Sequelize(databaseAddress, {
+        operatorsAliases: false
+      });
+      let { address } = event.pathParameters;
+      return getSavingsAccount(address).then(account => {
+        if (account) {
+          let query = `select sat.id as transactionId, sa.external_id as address, if(sat.transaction_type_enum = 1,'credit','debit') as creditOrDebit, sat.amount, sat.transaction_date as transactionDate, if(source.description is null, target.description, source.description) as additionalData from m_savings_account_transaction sat \
+          join m_savings_account sa on sa.id = sat.savings_account_id \
+          left outer join m_account_transfer_transaction source on source.from_savings_transaction_id = sat.id \
+          left outer join m_account_transfer_transaction target on target.to_savings_transaction_id = sat.id \
+          where sa.external_id = ${address} \
+          order by transactionId desc`
+          return sequelize.query(query).then(rows => {
+            return context.succeed({
+              statusCode: 200,
+              body: JSON.stringify({
+                items: rows[0],
+                count: rows[0].length
+              })
+            });
+          });
+        } else {
+          return context.succeed({
+            statusCode: 200,
+            body: JSON.stringify({
+              address,
+              items:[],
+              count: 0
+            })
+          });
+        }
+      });
+    })
+    .catch(context.fail);
+}
