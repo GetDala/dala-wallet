b36921900f8b793b008529ade604498bfd7e758b
Ross McEwan <rossmcewan@gmail.com>
2018-06-08 15:59:45 +0200

    share sequelize

--- src/support/steps.js
+++ src/support/steps.js
@@ -3,12 +3,13 @@ const documentClient = new AWS.DynamoDB.DocumentClient();
 const Sequelize = require('sequelize');
 const secretsClient = require('serverless-secrets/client');
 const secretsPromise = secretsClient.load();
+let sequelize;
 
-class NoMoreDataError extends Error{
-    constructor(){
-        super();
-        this.name = 'NoMoreDataError';
-    }
+class NoMoreDataError extends Error {
+  constructor() {
+    super();
+    this.name = 'NoMoreDataError';
+  }
 }
 
 exports.rewriteActivatedAccountWebhookEvents = async event => {
@@ -19,9 +20,11 @@ exports.rewriteActivatedAccountWebhookEvents = async event => {
   const databaseAddress = `mysql://${process.env.DALA_STORAGE_USERNAME}:${process.env.DALA_STORAGE_PASSWORD}@${process.env.DALA_STORAGE_CLUSTER}:${
     process.env.DALA_STORAGE_PORT
   }/mifostenant-default`;
-  const sequelize = new Sequelize(databaseAddress, {
-    operatorsAliases: false
-  });
+  if (!sequelize) {
+    sequelize = new Sequelize(databaseAddress, {
+      operatorsAliases: false
+    });
+  }
   let query = `select mc.id as clientId, mc.office_id as officeId, msa.id as resourceId, msa.id as savingsId, msa.activatedon_date from m_savings_account msa \
             join m_client mc on msa.client_id = mc.id \
             where msa.status_enum = 300 \
@@ -29,18 +32,21 @@ exports.rewriteActivatedAccountWebhookEvents = async event => {
             limit ${offset}, ${limit}`;
   let queryResults = await sequelize.query(query);
   let results = queryResults[0];
-  if(!results.length) throw new NoMoreDataError();
-  let batchPutResult = await documentClient.batchWrite({
-    RequestItems: {
-      FineractWebhookEvents: results.map(createPutRequests)
-    }
-  }).promise();
+  if (!results.length) throw new NoMoreDataError();
+  let batchPutResult = await documentClient
+    .batchWrite({
+      RequestItems: {
+        FineractWebhookEvents: results.map(createPutRequests)
+      }
+    })
+    .promise();
   console.log(batchPutResult);
   if (batchPutResult.UnprocessedItems && batchPutResult.UnprocessedItems.PutRequest && batchPutResult.UnprocessedItems.PutRequest.length) {
     //some items didn't process - rather process the whole batch again
     return { offset, limit };
   }
   offset += results.length;
+  await sequelize.close();
   return { offset, limit };
 };
 
